5529d5303a910e532e77d1a9d295c9799525a5fc
Lafu666 <3900116132@qq.com>
2026-08-02 13:35:50 +0800

    Create MainViewController.h

---
+++ Ares2ToolBox/ViewControllers/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  Ares2ToolBox
+//
+//  主界面控制器 - 文件选择/存档处理/数据库编辑入口
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
